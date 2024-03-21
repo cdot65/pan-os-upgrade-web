8c3cfa886c4a0dfcc0b76719c041b511ff422b02
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-21 08:43:56 -0500

    add frontend auth module and components

---
+++ frontend/src/app/auth/logout/logout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.scss'
+})
+export class LogoutComponent {
+
+}
