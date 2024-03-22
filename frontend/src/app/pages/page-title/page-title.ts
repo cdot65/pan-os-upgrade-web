51c3ec3089421d81bef2fc0cbbea5e1eaa8506ad
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-22 17:38:10 -0500

    Refactor homepage styling and page title service

--- frontend/src/app/pages/page-title/page-title.ts
+++ frontend/src/app/pages/page-title/page-title.ts
@@ -1,27 +1,27 @@
-import {Injectable} from '@angular/core';
-import {Title} from '@angular/platform-browser';
+import { Injectable } from "@angular/core";
+import { Title } from "@angular/platform-browser";
 
 /**
  * Service responsible for setting the title that appears above the components and guide pages.
  */
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class ComponentPageTitle {
-  _title = '';
-  _originalTitle = 'Angular Material UI component library';
+    _title = "";
+    _originalTitle = "PAN-OS Upgrade";
 
-  get title(): string {
-    return this._title;
-  }
-
-  set title(title: string) {
-    this._title = title;
-    if (title !== '') {
-      title = `${title} | Angular Material`;
-    } else {
-      title = this._originalTitle;
+    get title(): string {
+        return this._title;
     }
-    this.bodyTitle.setTitle(title);
-  }
 
-  constructor(private bodyTitle: Title) {}
+    set title(title: string) {
+        this._title = title;
+        if (title !== "") {
+            title = `${title} | PAN-OS Upgrade`;
+        } else {
+            title = this._originalTitle;
+        }
+        this.bodyTitle.setTitle(title);
+    }
+
+    constructor(private bodyTitle: Title) {}
 }
