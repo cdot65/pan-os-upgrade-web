4e5647b6db00c5d6d7eaa79a8f44408411891206
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-22 18:31:30 -0500

    Update button class names and theme in frontend code

--- frontend/src/app/shared/theme-picker/theme-storage/theme-storage.ts
+++ frontend/src/app/shared/theme-picker/theme-storage/theme-storage.ts
@@ -1,40 +1,40 @@
-import {Injectable, EventEmitter} from '@angular/core';
+import { EventEmitter, Injectable } from "@angular/core";
 
 export interface DocsSiteTheme {
-  name: string;
-  displayName?: string;
-  accent: string;
-  primary: string;
-  isDark?: boolean;
-  isDefault?: boolean;
+    name: string;
+    displayName?: string;
+    accent: string;
+    primary: string;
+    isDark?: boolean;
+    isDefault?: boolean;
 }
 
-
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class ThemeStorage {
-  static storageKey = 'docs-theme-storage-current-name';
+    static storageKey = "panosupgrade-theme-storage-current-name";
 
-  onThemeUpdate: EventEmitter<DocsSiteTheme> = new EventEmitter<DocsSiteTheme>();
+    onThemeUpdate: EventEmitter<DocsSiteTheme> =
+        new EventEmitter<DocsSiteTheme>();
 
-  storeTheme(theme: DocsSiteTheme) {
-    try {
-      window.localStorage[ThemeStorage.storageKey] = theme.name;
-    } catch { }
+    storeTheme(theme: DocsSiteTheme) {
+        try {
+            window.localStorage[ThemeStorage.storageKey] = theme.name;
+        } catch {}
 
-    this.onThemeUpdate.emit(theme);
-  }
-
-  getStoredThemeName(): string | null {
-    try {
-      return window.localStorage[ThemeStorage.storageKey] || null;
-    } catch {
-      return null;
+        this.onThemeUpdate.emit(theme);
     }
-  }
 
-  clearStorage() {
-    try {
-      window.localStorage.removeItem(ThemeStorage.storageKey);
-    } catch { }
-  }
+    getStoredThemeName(): string | null {
+        try {
+            return window.localStorage[ThemeStorage.storageKey] || null;
+        } catch {
+            return null;
+        }
+    }
+
+    clearStorage() {
+        try {
+            window.localStorage.removeItem(ThemeStorage.storageKey);
+        } catch {}
+    }
 }
