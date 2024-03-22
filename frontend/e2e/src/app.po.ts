09d1ac3c93ddd68205e088df4ed9d24ed2137e93
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-22 17:40:35 -0500

    Fix formatting issues in SCSS files

--- frontend/e2e/src/app.po.ts
+++ frontend/e2e/src/app.po.ts
@@ -1,12 +1,13 @@
-import {browser, by, element} from 'protractor';
+import { browser, by, element } from "protractor";
 
 export class MaterialDocsAppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
+    navigateTo() {
+        return browser.get(browser.baseUrl) as Promise<any>;
+    }
 
-  getTitleText() {
-    return element(by.css('app-homepage header .docs-header-headline .mat-h1'))
-      .getText() as Promise<string>;
-  }
+    getTitleText() {
+        return element(
+            by.css("app-homepage header .panosupgrade-header-headline .mat-h1")
+        ).getText() as Promise<string>;
+    }
 }
