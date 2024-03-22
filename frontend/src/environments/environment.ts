d178a391963cb5c95037d7925f523fcca5e5028d
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-22 09:10:29 -0500

    Update package.json and index.html

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://localhost:8000",
+  tokenUrl: "/api/v1/dj-rest-auth/login/",
+  envVar: {
+    OPENAI_API_KEY: "sk-123456117891234567891234512345678912345678912345",
+  },
+};
