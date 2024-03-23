6b075651aba80def1eac7fadd4894fde1b51b7ef
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-23 07:25:49 -0500

    Refactor unregisterServiceWorkers function

--- frontend/src/unregister-service-workers.ts
+++ frontend/src/unregister-service-workers.ts
@@ -3,15 +3,14 @@
  * whether there were service workers registered or not.
  */
 export async function unregisterServiceWorkers(): Promise<boolean> {
-  if (!navigator.serviceWorker) {
-    return false;
-  }
+    if (!navigator.serviceWorker) {
+        return false;
+    }
 
-  const registrations = await navigator.serviceWorker.getRegistrations();
+    const registrations = await navigator.serviceWorker.getRegistrations();
 
-  // Walk through every currently registered Service Worker and unregister it. There can be
-  // service workers from previous versions of the Angular Material docs.
-  registrations.forEach(registration => registration.unregister());
+    // Walk through every currently registered Service Worker and unregister it.
+    registrations.forEach((registration) => registration.unregister());
 
-  return registrations.length > 0;
+    return registrations.length > 0;
 }
