c5e504fadab9a2ea662032560366df3c4b36b43c
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-23 06:51:39 -0500

    Remove unused files and dependencies

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -11,7 +11,7 @@
  * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
  * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
- * Learn more in https://angular.io/guide/browser-support
+ * Learn more in https://github.com/cdot65/pan-os-upgrade-web/guide/browser-support
  */
 
 /***************************************************************************************************
@@ -58,8 +58,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-
+import "zone.js"; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
@@ -68,4 +67,4 @@ import 'zone.js';  // Included with Angular CLI.
 // Examples might use `$localize`, so we initialize the localize package. Note that we do not
 // intend to load any translations at runtime. `$localize` has a pass-through mode where it does
 // not actually translate content. This is what we want to use for our examples.
-import '@angular/localize/init';
+import "@angular/localize/init";
--- frontend/tools/npm/check-npm.js
+++ frontend/tools/npm/check-npm.js
@@ -3,7 +3,7 @@
  * Copyright cdot65 All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://github.com/cdot65/pan-os-upgrade-web/license
  */
 
 if (process.env.npm_execpath.indexOf("yarn") === -1) {
