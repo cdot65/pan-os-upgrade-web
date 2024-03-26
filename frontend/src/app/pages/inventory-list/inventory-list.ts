b9d87f9fc60456acfecde991340cd2857046e90d
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-03-26 04:37:19 -0500

    Refactor inventory list component to use Angular Material table and sorting

--- frontend/src/app/pages/inventory-list/inventory-list.ts
+++ frontend/src/app/pages/inventory-list/inventory-list.ts
@@ -1,38 +1,62 @@
-// src/app/pages/inventory-list/inventory-list.ts
-
-import { Component, HostBinding, OnInit } from "@angular/core";
-import { Router, RouterLink } from "@angular/router";
+import {
+    AfterViewInit,
+    Component,
+    HostBinding,
+    OnInit,
+    ViewChild,
+} from "@angular/core";
+import { MatSort, MatSortModule, Sort } from "@angular/material/sort";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
 
 import { Firewall } from "../../shared/interfaces/firewall.interface";
 import { InventoryService } from "../../shared/services/inventory.service";
-import { MatCardModule } from "@angular/material/card";
+import { LiveAnnouncer } from "@angular/cdk/a11y";
 import { NgFor } from "@angular/common";
 import { Panorama } from "../../shared/interfaces/panorama.interface";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-inventory-list",
     templateUrl: "./inventory-list.html",
     styleUrls: ["./inventory-list.scss"],
     standalone: true,
-    imports: [NgFor, RouterLink, MatCardModule],
+    imports: [NgFor, MatTableModule, MatSortModule],
 })
-export class InventoryList implements OnInit {
+export class InventoryList implements OnInit, AfterViewInit {
     @HostBinding("class.main-content") readonly mainContentClass = true;
     inventoryItems: (Firewall | Panorama)[] = [];
+    displayedColumns: string[] = [
+        "hostname",
+        "ipAddress",
+        "platform",
+        "inventoryType",
+        "notes",
+    ];
+    dataSource: MatTableDataSource<Firewall | Panorama> =
+        new MatTableDataSource<Firewall | Panorama>([]);
+
+    @ViewChild(MatSort) sort: MatSort = new MatSort();
 
     constructor(
         private inventoryService: InventoryService,
         private router: Router,
+        private _liveAnnouncer: LiveAnnouncer,
     ) {}
 
     ngOnInit(): void {
         this.getInventoryItems();
     }
 
+    ngAfterViewInit() {
+        this.dataSource.sort = this.sort;
+    }
+
     getInventoryItems(): void {
         this.inventoryService.fetchInventoryData().subscribe(
             (items) => {
                 this.inventoryItems = items;
+                this.dataSource = new MatTableDataSource(this.inventoryItems);
+                this.dataSource.sort = this.sort;
             },
             (error) => {
                 console.error("Error fetching inventory items:", error);
@@ -40,7 +64,15 @@ export class InventoryList implements OnInit {
         );
     }
 
-    onItemClick(item: Firewall | Panorama): void {
+    onRowClick(item: Firewall | Panorama): void {
         this.router.navigate(["/inventory", item.uuid]);
+    }
+
+    announceSortChange(sortState: Sort) {
+        if (sortState.direction) {
+            this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+        } else {
+            this._liveAnnouncer.announce("Sorting cleared");
+        }
     }
 }
