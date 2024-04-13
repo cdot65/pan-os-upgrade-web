5d96c923fe0645f3145c8bd513a54512a46e17d7
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-04-12 23:40:10 -0500

    Refactor inventory sync form interface and related code

--- frontend/src/app/shared/interfaces/device-sync-form.interface.ts
+++ frontend/src/app/shared/interfaces/device-sync-form.interface.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-// frontend/src/app/shared/interfaces/inventory-sync-form.interface.ts
+// frontend/src/app/shared/interfaces/device-sync-form.interface.ts
 
-export interface InventorySyncForm {
+export interface DeviceSyncForm {
     author: number;
     panorama_device: string;
     profile: string;
