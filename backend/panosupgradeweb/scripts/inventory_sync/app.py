76da84dde22b71fbbc218977128eeeb0f785ac53
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-04-22 07:57:53 -0500

    move logging to logstash

--- backend/panosupgradeweb/scripts/inventory_sync/app.py
+++ backend/panosupgradeweb/scripts/inventory_sync/app.py
@@ -7,6 +7,7 @@ import logging
 import xml.etree.ElementTree as ET
 
 import django
+from logstash_async.handler import AsynchronousLogstashHandler
 
 from panos.firewall import Firewall
 from panos.panorama import Panorama
@@ -25,6 +26,40 @@ django.setup()
 # import our Django models
 from panosupgradeweb.models import Device, DeviceType, Profile  # noqa: E402
 from django.core.exceptions import ObjectDoesNotExist  # noqa: E402
+
+
+# Create a logger instance
+logger = logging.getLogger("pan-os-inventory")
+logger.setLevel(logging.DEBUG)
+
+# Create a Logstash handler
+logstash_handler = AsynchronousLogstashHandler(
+    # Use the Logstash service name from the Docker Compose file
+    host="logstash",
+    # The port Logstash is listening on
+    port=5000,
+    # Disable the local queue database
+    database_path=None,
+)
+
+# Add the Logstash handler to the logger
+logger.addHandler(logstash_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+logger.addHandler(console_handler)
+
+
+def log_inventory_sync(
+    job_id: str,
+    level: str,
+    message: str,
+):
+    extra = {
+        "job_id": job_id,
+        "job_type": "inventory_sync",
+    }
+    logger.log(getattr(logging, level), message, extra=extra)
 
 
 def find_devicegroup_by_serial(data, serial):
@@ -90,18 +125,47 @@ def get_device_group_mapping(pan: Panorama):
 
 
 def run_inventory_sync(
-    panorama_device_uuid,
-    profile_uuid,
-    author_id,
+    author_id: int,
+    job_id: str,
+    panorama_device_uuid: str,
+    profile_uuid: str,
 ):
-    logging.debug(f"Running inventory sync for Panorama device: {panorama_device_uuid}")
-    logging.debug(f"Using profile: {profile_uuid}")
-    logging.debug(f"Author ID: {author_id}")
+    log_inventory_sync(
+        job_id=job_id,
+        level="INFO",
+        message=f"Running inventory sync for Panorama device: {panorama_device_uuid}",
+    )
+    log_inventory_sync(
+        job_id=job_id,
+        level="INFO",
+        message=f"Using profile: {profile_uuid}",
+    )
+    log_inventory_sync(
+        job_id=job_id,
+        level="INFO",
+        message=f"Author ID: {author_id}",
+    )
 
+    log_inventory_sync(
+        job_id=job_id,
+        level="INFO",
+        message="Starting inventory sync",
+    )
     try:
         # Retrieve the Panorama device and profile objects from the database
         panorama_device = Device.objects.get(uuid=panorama_device_uuid)
+        log_inventory_sync(
+            job_id=job_id,
+            level="DEBUG",
+            message=f"Retrieved Panorama device: {panorama_device.hostname}",
+        )
+
         profile = Profile.objects.get(uuid=profile_uuid)
+        log_inventory_sync(
+            job_id=job_id,
+            level="DEBUG",
+            message=f"Retrieved profile: {profile.name}",
+        )
 
         # Access the authentication data directly from the profile fields
         pan_username = profile.pan_username
@@ -117,17 +181,37 @@ def run_inventory_sync(
             pan_username,
             pan_password,
         )
+        log_inventory_sync(
+            job_id=job_id,
+            level="DEBUG",
+            message=f"Connected to Panorama device: {panorama_device.ipv4_address}",
+        )
 
         # Retrieve the system information from the Panorama device
         system_info = pan.show_system_info()
         panorama_hostname = system_info["system"]["hostname"]
+        log_inventory_sync(
+            job_id=job_id,
+            level="DEBUG",
+            message=f"Connected to Panorama device: {panorama_hostname}",
+        )
 
         # Retrieve the device group mappings from the Panorama device
         device_group_mappings = get_device_group_mapping(pan)
+        log_inventory_sync(
+            job_id=job_id,
+            level="DEBUG",
+            message=f"Retrieved device group mappings {device_group_mappings}",
+        )
 
         # Retrieve the connected devices
         connected_devices = pan.op("show devices connected")
         result_dict = flatten_xml_to_dict(element=connected_devices)
+        log_inventory_sync(
+            job_id=job_id,
+            level="DEBUG",
+            message=f"Retrieved connected devices {result_dict['result']['devices']}",
+        )
 
         # Convert the result dictionary to JSON
         json_output = json.dumps(result_dict)
@@ -135,6 +219,11 @@ def run_inventory_sync(
         # Create a list of peer firewalls missing their HA partner
         missing_peer_devices = []
 
+        log_inventory_sync(
+            job_id=job_id,
+            level="INFO",
+            message="Starting device creation/update",
+        )
         # Step 1: Create or update Device objects based on the retrieved devices
         for device in result_dict["result"]["devices"]["entry"]:
             platform_name = device["model"]
@@ -148,6 +237,11 @@ def run_inventory_sync(
                     f"Platform '{platform_name}' not found. Skipping device: {device['@name']}"
                 )
                 continue
+            log_inventory_sync(
+                job_id=job_id,
+                level="DEBUG",
+                message=f"Retrieved platform: {platform.name}",
+            )
 
             # Build connection to firewall device through Panorama object
             firewall = Firewall(serial=device["serial"])
@@ -155,9 +249,24 @@ def run_inventory_sync(
 
             # Retrieve the system information from the firewall device
             info = firewall.show_system_info()
+            log_inventory_sync(
+                job_id=job_id,
+                level="INFO",
+                message=f"Retrieved system info for device: {info['system']['hostname']}",
+            )
+            log_inventory_sync(
+                job_id=job_id,
+                level="DEBUG",
+                message=f"System info: {info}",
+            )
 
             # Retrieve the HA state information from the firewall device
             ha_info = firewall.show_highavailability_state()
+            log_inventory_sync(
+                job_id=job_id,
+                level="INFO",
+                message=f"Retrieved HA state for device: {info['system']['hostname']}",
+            )
 
             ha_enabled = ha_info[0] != "disabled"
 
@@ -168,6 +277,11 @@ def run_inventory_sync(
 
             if ha_enabled:
                 ha_details = flatten_xml_to_dict(element=ha_info[1])
+                log_inventory_sync(
+                    job_id=job_id,
+                    level="DEBUG",
+                    message=f"HA details: {ha_details}",
+                )
                 peer_ip = ha_details["result"]["group"]["peer-info"]["mgmt-ip"].split(
                     "/"
                 )[0]
@@ -190,6 +304,11 @@ def run_inventory_sync(
                 try:
                     panorama_appliance = Device.objects.get(hostname=panorama_hostname)
                     panorama_appliance_uuid = str(panorama_appliance.uuid)
+                    log_inventory_sync(
+                        job_id=job_id,
+                        level="DEBUG",
+                        message=f"Retrieved Panorama appliance: {panorama_appliance.hostname}",
+                    )
                 except ObjectDoesNotExist:
                     logging.warning(
                         f"Panorama appliance '{panorama_hostname}' not found. Skipping assignment."
@@ -231,8 +350,18 @@ def run_inventory_sync(
                     "uptime": info["system"]["uptime"],
                 },
             )
+            log_inventory_sync(
+                job_id=job_id,
+                level="INFO",
+                message=f"Created/updated device: {info['system']['hostname']}",
+            )
 
         # Step 2: Revisit the missing peer devices and update the peer_device_id
+        log_inventory_sync(
+            job_id=job_id,
+            level="INFO",
+            message="Starting missing peer device update",
+        )
         for firewall in missing_peer_devices:
             ha_info = firewall.show_highavailability_state()
             ha_details = flatten_xml_to_dict(element=ha_info[1])
@@ -244,6 +373,11 @@ def run_inventory_sync(
                 peer_device = Device.objects.get(ipv4_address=peer_ip)
                 peer_device_uuid = str(peer_device.uuid)
                 peer_state = ha_details["result"]["group"]["peer-info"]["state"]
+                log_inventory_sync(
+                    job_id=job_id,
+                    level="DEBUG",
+                    message=f"Retrieved peer device: {peer_device.hostname}",
+                )
             except ObjectDoesNotExist:
                 logging.error(
                     f"Peer device with IP {peer_ip} still not found. Skipping HA deployment."
@@ -252,6 +386,11 @@ def run_inventory_sync(
 
             Device.objects.filter(serial=firewall.serial).update(
                 peer_device_id=peer_device_uuid, peer_ip=peer_ip, peer_state=peer_state
+            )
+            log_inventory_sync(
+                job_id=job_id,
+                level="INFO",
+                message=f"Updated peer device for: {firewall.serial}",
             )
         return json_output
 
@@ -292,4 +431,9 @@ if __name__ == "__main__":
     profile_uuid = args.profile_uuid
     author_id = args.author_id
 
-    run_inventory_sync(panorama_device_uuid, profile_uuid, author_id)
+    run_inventory_sync(
+        author_id=author_id,
+        job_id="CLI",
+        panorama_device_uuid=panorama_device_uuid,
+        profile_uuid=profile_uuid,
+    )
