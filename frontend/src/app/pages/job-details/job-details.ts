8abc4ed393ce1ebf24f0ef935a5f563b78d4b6c7
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-04-23 10:58:27 -0500

    Add ElasticsearchService import and searchLogsByJobId method to job-details.ts

--- frontend/src/app/pages/job-details/job-details.ts
+++ frontend/src/app/pages/job-details/job-details.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Component, HostBinding, OnDestroy, OnInit } from "@angular/core";
+
 import { ActivatedRoute } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ComponentPageTitle } from "../page-title/page-title";
+import { ElasticsearchService } from "../../shared/services/elasticsearch.service";
 import { Footer } from "src/app/shared/footer/footer";
 import { Job } from "../../shared/interfaces/job.interface";
 import { JobPageHeader } from "../job-page-header/job-page-header";
@@ -34,11 +36,13 @@ export class JobDetailsComponent implements OnDestroy, OnInit {
     @HostBinding("class.main-content") readonly mainContentClass = true;
     jobItem: Job | undefined;
     jobId: string | null = null;
+    logs: any[] = [];
     parsedJsonData: any;
     private destroy$ = new Subject<void>();
 
     constructor(
         private jobService: JobService,
+        private elasticsearchService: ElasticsearchService,
         private route: ActivatedRoute,
         private snackBar: MatSnackBar,
         public _componentPageTitle: ComponentPageTitle,
@@ -52,6 +56,7 @@ export class JobDetailsComponent implements OnDestroy, OnInit {
                 (item: Job) => {
                     this.jobItem = item;
                     this.parsedJsonData = JSON.parse(item.json_data);
+                    this.searchLogsByJobId(item.task_id);
                 },
                 (error: any) => {
                     console.error("Error fetching job item:", error);
@@ -64,6 +69,24 @@ export class JobDetailsComponent implements OnDestroy, OnInit {
                     );
                 },
             );
+    }
+
+    async searchLogsByJobId(jobId: string) {
+        try {
+            const result = await this.elasticsearchService
+                .getLogsByJobId(jobId)
+                .toPromise();
+            this.logs = result.hits.hits;
+        } catch (error) {
+            console.error("Error searching logs by job ID:", error);
+            this.snackBar.open(
+                "Failed to fetch logs. Please try again.",
+                "Close",
+                {
+                    duration: 3000,
+                },
+            );
+        }
     }
 
     ngOnDestroy(): void {
