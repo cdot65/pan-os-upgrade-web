aa1a6a7ed2642afb6416bb7eddb7734718918eff
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-04-29 12:59:38 -0500

    Refactor UpgradeResponse interface to use upgrade_jobs instead of job_ids for returning upgrade job information

--- frontend/src/app/shared/interfaces/upgrade-response.interface.ts
+++ frontend/src/app/shared/interfaces/upgrade-response.interface.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 // src/app/shared/interfaces/upgrade-response.interface.ts
 
+export interface UpgradeJob {
+    hostname: string;
+    job: string;
+}
+
 export interface UpgradeResponse {
-    job_ids: string[];
+    upgrade_jobs: UpgradeJob[];
 }
