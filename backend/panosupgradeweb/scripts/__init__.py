156791a97fffbe53a96f61d1437dda03470430bd
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-05-05 12:14:16 -0500

    Refactor PAN-OS upgrade process to use main function for running the upgrade

--- backend/panosupgradeweb/scripts/__init__.py
+++ backend/panosupgradeweb/scripts/__init__.py
@@ -2,4 +2,4 @@
 
 from .inventory_sync.app import run_inventory_sync
 from .device_refresh.app import run_device_refresh
-from .upgrade.app import run_panos_upgrade
+from .upgrade.app import main as run_panos_upgrade
