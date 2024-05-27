53d6cf24a2487bfb5e1c52e5d7281da7bd6ec30f
Calvin Remsburg <calvin@cdot.io>
2024-05-27 17:51:19 -0500

    add custom errors, LoginResponse, MatSnackBar, defined api endpoints, added docstrings, new private functions getAuthHeaders, handleError, added retryWhen for server errors, error handling to other functions, updated state of isLoggedInSubject to false when logging out

--- frontend/src/app/shared/services/auth.service.ts
+++ frontend/src/app/shared/services/auth.service.ts
@@ -1,10 +1,23 @@
+/* eslint-disable max-len */
 // src/app/shared/services/auth.service.ts
 
-import { BehaviorSubject, Observable } from "rxjs";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { map, tap } from "rxjs/operators";
+// Import your custom error classes
+import {
+    AuthError,
+    InvalidCredentialsError,
+    RegistrationError,
+} from "../errors/auth.error";
+import { BehaviorSubject, Observable, throwError, timer } from "rxjs";
+import {
+    HttpClient,
+    HttpErrorResponse,
+    HttpHeaders,
+} from "@angular/common/http";
+import { catchError, map, mergeMap, retryWhen, tap } from "rxjs/operators";
 
 import { Injectable } from "@angular/core";
+import { LoginResponse } from "../interfaces/login-response";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { Router } from "@angular/router";
 import { environment } from "../../../environments/environment";
 
@@ -12,23 +25,118 @@ import { environment } from "../../../environments/environment";
     providedIn: "root",
 })
 export class AuthService {
+    // Define the API endpoints
     private apiUrl = environment.apiUrl;
-    private tokenUrl = this.apiUrl + environment.tokenUrl;
-    private registrationUrl =
-        environment.apiUrl + "/api/v1/dj-rest-auth/registration/";
-    private userProfileUrl = environment.apiUrl + "/api/v1/dj-rest-auth/user/";
+    private apiEndpointToken = this.apiUrl + environment.apiEndpointToken;
+    private apiEndpointRegistration = `${this.apiUrl}/api/v1/dj-rest-auth/registration/`;
+    private apiEndpointUserProfile = `${this.apiUrl}/api/v1/dj-rest-auth/user/`;
+
+    // BehaviorSubject to keep track of the login status
     private isLoggedInSubject = new BehaviorSubject<boolean>(false);
     isLoggedIn$: Observable<boolean>;
 
+    /**
+     * Represents the AuthService class that handles authentication-related functionality.
+     */
     constructor(
         private http: HttpClient,
         private router: Router,
+        private snackBar: MatSnackBar,
     ) {
         this.isLoggedIn$ = this.isLoggedInSubject.asObservable();
         const token = localStorage.getItem("auth_token");
         this.isLoggedInSubject.next(!!token);
     }
 
+    /**
+     * Retrieves the HttpHeaders object with the Authorization token.
+     * If a token is provided, it will be used. Otherwise, it will attempt to retrieve the token from local storage.
+     * If no token is found, an empty HttpHeaders object will be returned.
+     *
+     * @param token - The authorization token (optional)
+     * @returns The HttpHeaders object with the Authorization token
+     */
+    private getAuthHeaders(token: string | null = null): HttpHeaders {
+        const authToken = token || localStorage.getItem("auth_token");
+        if (authToken) {
+            return new HttpHeaders().set("Authorization", `Token ${authToken}`);
+        } else {
+            // Return empty headers if no token
+            return new HttpHeaders();
+        }
+    }
+
+    /**
+     * Handles the error response from an HTTP request.
+     *
+     * @param error - The error response from the HTTP request.
+     * @returns An Observable that emits the error.
+     */
+    private handleError(error: HttpErrorResponse): Observable<never> {
+        let authError: AuthError;
+
+        // Check if the error is a client-side or network error
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred.
+            authError = new AuthError(
+                error.error,
+                `An error occurred: ${error.error.message}`,
+            );
+        } else {
+            // The backend returned an unsuccessful response code.
+            switch (error.status) {
+                case 400: {
+                    // Check for the specific login error message
+                    if (
+                        error.error &&
+                        error.error.non_field_errors &&
+                        error.error.non_field_errors.includes(
+                            "Unable to log in with provided credentials.",
+                        )
+                    ) {
+                        // Extract the specific error message from the response payload
+                        const errorMessage = error.error.non_field_errors[0];
+                        authError = new InvalidCredentialsError(
+                            error,
+                            errorMessage,
+                        );
+                    } else if (
+                        error.url?.includes(this.apiEndpointRegistration)
+                    ) {
+                        authError = new RegistrationError(error);
+                    } else {
+                        authError = new AuthError(
+                            error,
+                            `Bad Request: ${JSON.stringify(error.error)}`,
+                        );
+                    }
+                    break;
+                }
+                default:
+                    authError = new AuthError(
+                        error,
+                        `Backend returned code ${error.status}, body was: ${JSON.stringify(error.error)}`,
+                    );
+            }
+        }
+
+        // Display the error message in a snackbar
+        this.snackBar.open(authError.message, "Close", {
+            duration: 5000,
+            verticalPosition: "bottom",
+        });
+
+        // Return the error
+        return throwError(() => authError);
+    }
+
+    /**
+     * Logs in the user with the provided username and password.
+     *
+     * @param username - The username of the user.
+     * @param password - The password of the user.
+     * @returns An Observable that emits the response from the login request.
+     */
     login(username: string, password: string) {
         const headers = new HttpHeaders().set(
             "Content-Type",
@@ -36,20 +144,43 @@ export class AuthService {
         );
         const body = JSON.stringify({ username, password });
 
-        return this.http.post<any>(`${this.tokenUrl}`, body, { headers }).pipe(
-            tap((response) => {
-                if (response && response.key) {
-                    // Check if the token is present in the response
-                    localStorage.setItem("auth_token", response.key);
-                    localStorage.setItem("author", response.author);
-                    this.isLoggedInSubject.next(true);
-                } else {
-                    console.error("Token not found in the response");
-                }
-            }),
-        );
+        return this.http
+            .post<LoginResponse>(this.apiEndpointToken, body, { headers })
+            .pipe(
+                tap((response) => {
+                    if (response && response.key) {
+                        localStorage.setItem("auth_token", response.key);
+                        localStorage.setItem("author", response.author);
+                        this.isLoggedInSubject.next(true);
+                    }
+                }),
+                retryWhen((errors) =>
+                    errors.pipe(
+                        mergeMap((error: HttpErrorResponse, i) => {
+                            const retryAttempt = i + 1;
+                            if (retryAttempt <= 3 && error.status >= 500) {
+                                const delayTime =
+                                    Math.pow(2, retryAttempt) * 1000;
+                                return timer(delayTime);
+                            } else {
+                                return throwError(error);
+                            }
+                        }),
+                    ),
+                ),
+                catchError(this.handleError.bind(this)),
+            );
     }
 
+    /**
+     * Registers a new user.
+     *
+     * @param username - The username of the user.
+     * @param email - The email address of the user.
+     * @param password1 - The password of the user.
+     * @param password2 - The confirmation password of the user.
+     * @returns An Observable that emits the response from the server.
+     */
     register(
         username: string,
         email: string,
@@ -61,32 +192,50 @@ export class AuthService {
             "application/json",
         );
         const body = JSON.stringify({ username, email, password1, password2 });
-        return this.http.post<any>(this.registrationUrl, body, { headers });
+        return this.http
+            .post<any>(this.apiEndpointRegistration, body, { headers })
+            .pipe(catchError(this.handleError.bind(this)));
     }
 
+    /**
+     * Retrieves the user profile from the server.
+     *
+     * @returns An Observable that emits the user profile data.
+     */
     getUserProfile() {
-        return this.http.get<any>(this.userProfileUrl);
+        return this.http
+            .get<any>(this.apiEndpointUserProfile, {
+                headers: this.getAuthHeaders(),
+            })
+            .pipe(catchError(this.handleError.bind(this)));
     }
 
+    /**
+     * Logs out the user by removing the authentication token from local storage,
+     * updating the login status, and navigating to the login page.
+     */
     logout(): void {
         localStorage.removeItem("auth_token");
+        this.isLoggedInSubject.next(false);
         this.router.navigate(["/auth/login"]);
     }
 
+    /**
+     * Retrieves user data from the server.
+     *
+     * @returns An Observable that emits the user data.
+     */
     getUserData() {
-        const authToken = localStorage.getItem("auth_token");
-        const headers = new HttpHeaders().set(
-            "Authorization",
-            `Token ${authToken}`,
-        );
-
         return this.http
-            .get(`${this.apiUrl}/api/v1/dj-rest-auth/user/`, { headers })
+            .get(this.apiEndpointUserProfile, {
+                headers: this.getAuthHeaders(),
+            })
             .pipe(
                 map((response: any) => ({
                     ...response,
                     pk: Number(response.pk),
                 })),
+                catchError(this.handleError.bind(this)),
             );
     }
 }
