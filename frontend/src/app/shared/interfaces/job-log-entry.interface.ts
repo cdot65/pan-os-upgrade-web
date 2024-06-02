c7a54bc5ae2054176121369d51a69945e5de371f
Calvin Remsburg <calvin@cdot.io>
2024-06-02 17:48:12 -0500

    refactor: Add JobLogEntry interface for logging job details and severity levels

---
+++ frontend/src/app/shared/interfaces/job-log-entry.interface.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// src/app/shared/interfaces/job-log-entry.ts
+
+export interface JobLogEntry {
+    timestamp: string;
+    severity_level: string;
+    message: string;
+}
