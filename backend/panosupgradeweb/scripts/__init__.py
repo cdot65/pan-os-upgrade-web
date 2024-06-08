014e055ac710a303c72334ce57fae2fe9781d7b3
Calvin Remsburg <calvin@cdot.io>
2024-06-08 08:00:31 -0500

    rename primary function to main

--- backend/panosupgradeweb/scripts/__init__.py
+++ backend/panosupgradeweb/scripts/__init__.py
@@ -1,5 +1,5 @@
 # backend/panosupgradeweb/scripts/__init__.py
 
-from .inventory_sync.app import run_inventory_sync
 from .device_refresh.app import run_device_refresh
+from .inventory_sync.app import main as run_inventory_sync
 from .upgrade.app import main as run_panos_upgrade
