82b8ab5dc70534664afe5ff6e352085831b6e77d
Calvin Remsburg <calvin@cdot.io>
2024-06-08 11:14:24 -0500

    Renamed `run_panos_upgrade` to `run_upgrade_device`
    Renamed two major components in the code, `run_panos_upgrade` to `run_upgrade_device` and also the method `run_device_refresh` to `main`. The renamed functions are aimed at enhancing code readability and better reflect the operations they perform. There were also corresponding changes to the script imports and method call in tasks.py.

--- backend/panosupgradeweb/scripts/__init__.py
+++ backend/panosupgradeweb/scripts/__init__.py
@@ -1,5 +1,5 @@
 # backend/panosupgradeweb/scripts/__init__.py
 
-from .device_refresh.app import run_device_refresh
+from .device_refresh.app import main as run_device_refresh
 from .inventory_sync.app import main as run_inventory_sync
-from .upgrade.app import main as run_panos_upgrade
+from .upgrade_device.app import main as run_upgrade_device
