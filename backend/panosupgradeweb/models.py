511fb1b592adb3789c5f33f4c9456754cb916176
Calvin Remsburg <calvin@cdot.io>
2024-06-11 06:56:32 -0500

    Add Snapshot, ContentVersion, License, NetworkInterface models
    This commit has added four new models to the panosupgradeweb application. These are: Snapshot, ContentVersion, License, and NetworkInterface. It has also removed the unused import "Q" from the django.db.models module.

--- backend/panosupgradeweb/models.py
+++ backend/panosupgradeweb/models.py
@@ -3,7 +3,6 @@
 import uuid
 from django.conf import settings
 from django.db import models
-from django.db.models import Q
 
 
 class DeviceType(models.Model):
@@ -366,3 +365,41 @@ class Profile(models.Model):
         editable=False,
         verbose_name="UUID",
     )
+
+
+class Snapshot(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name="snapshots")
+    device = models.ForeignKey(
+        Device, on_delete=models.CASCADE, related_name="snapshots"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ContentVersion(models.Model):
+    snapshot = models.OneToOneField(
+        Snapshot, on_delete=models.CASCADE, related_name="content_version"
+    )
+    version = models.CharField(max_length=100)
+
+
+class License(models.Model):
+    snapshot = models.ForeignKey(
+        Snapshot, on_delete=models.CASCADE, related_name="licenses"
+    )
+    feature = models.CharField(max_length=100)
+    description = models.TextField()
+    serial = models.CharField(max_length=100)
+    issued = models.CharField(max_length=100)
+    expires = models.CharField(max_length=100)
+    expired = models.CharField(max_length=10)
+    base_license_name = models.CharField(max_length=100)
+    authcode = models.CharField(max_length=100, null=True, blank=True)
+    custom = models.JSONField(null=True, blank=True)
+
+
+class NetworkInterface(models.Model):
+    snapshot = models.ForeignKey(
+        Snapshot, on_delete=models.CASCADE, related_name="network_interfaces"
+    )
+    name = models.CharField(max_length=100)
+    status = models.CharField(max_length=10)
