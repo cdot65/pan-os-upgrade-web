4182cf8c9365a1ad43031181da0c756934843b05
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-06-20 13:00:54 -0500

    Add PanosVersion import to models init
    This commit includes the import of the PanosVersion from the devices

--- backend/panosupgradeweb/models/__init__.py
+++ backend/panosupgradeweb/models/__init__.py
@@ -1,4 +1,4 @@
 from .snapshots import Snapshot, ContentVersion, License, NetworkInterface
-from .devices import Device, DeviceType
+from .devices import Device, DeviceType, PanosVersion
 from .jobs import Job, JobLogEntry
 from .profiles import Profile
