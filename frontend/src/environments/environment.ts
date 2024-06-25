1dd45adba415ff30a19e48a2e25846ba36d3f2b9
Calvin Remsburg <calvin@cdot.io>
2024-06-25 12:24:36 -0500

    Update apiUrl in environment configuration
    The apiUrl in the environment configuration was a static localhost address. This has been replaced with a configurable API_URL, making it easier to switch between different API addresses. This is important especially when moving between production and development environments.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
+// src/environments/environment.ts and environment.prod.ts
 export const environment = {
-    production: false,
-    apiUrl: "http://localhost:8000",
+    production: false, // or true for environment.prod.ts
+    apiUrl: "${API_URL}",
     apiEndpointToken: "/api/v1/dj-rest-auth/login/",
 };
