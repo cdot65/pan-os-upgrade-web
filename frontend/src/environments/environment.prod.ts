cebe7e42b3b08ca1a3d7266b77064cc6d3c8f716
Calvin Remsburg <calvin@cdot.io>
2024-06-30 07:21:39 -0500

    Update apiUrl and remove backendPort in environments
    The apiUrl in both environment.prod.ts and environment.ts files has been set explicitly to "http://localhost:8000". The variable backendPort has been also removed from both files, as it seems to be redundant or unnecessary now.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: "${API_URL}",
-  backendPort: "${BACKEND_PORT}",
-  apiEndpointToken: "/api/v1/dj-rest-auth/login/",
+    production: true,
+    apiUrl: "http://localhost:8000",
+    apiEndpointToken: "/api/v1/dj-rest-auth/login/",
 };
