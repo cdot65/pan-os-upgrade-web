858d335c546eab898f52aa9babc5dfea3a5e3fa8
Calvin Remsburg <calvin@cdot.io>
2024-06-30 06:25:17 -0500

    Refactor codebase and update deployment configurations
    This commit updates internal references to handle environment variables more efficiently. It modifies the way `API_URL` and `BACKEND_PORT` are handled and removed the `CELERY_BROKER_URL` from Django settings. Several deployment template files in the helm/pan-os-upgrade-web directory have been updated for better integrations.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
-// src/environments/environment.ts and environment.prod.ts
 export const environment = {
-    production: false, // or true for environment.prod.ts
-    apiUrl: "${API_URL}",
-    apiEndpointToken: "/api/v1/dj-rest-auth/login/",
+  production: true,
+  apiUrl: "${API_URL}",
+  backendPort: "${BACKEND_PORT}",
+  apiEndpointToken: "/api/v1/dj-rest-auth/login/",
 };
