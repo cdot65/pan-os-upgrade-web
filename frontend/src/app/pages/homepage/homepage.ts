bd851bf93004b17e61cd10be81a46f2e134888fa
Calvin Remsburg <calvin@cdot.io>
2024-07-01 06:48:02 -0500

    Add dashboard features to homepage component
    The changes include the addition of several new methods and properties to the homepage component to accommodate for dashboard features. This involves the integration of app version, job status, and PAN-OS version chart displays. Additional methods for retrieving and processing job and device data have been implemented. The homepage now also implements OnDestroy lifecycle hook to manage component cleanup.

--- frontend/src/app/pages/homepage/homepage.ts
+++ frontend/src/app/pages/homepage/homepage.ts
@@ -1,13 +1,18 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable max-len */
 // src/app/pages/homepage/homepage.ts
 
 import {
     Component,
     HostBinding,
     Inject,
+    OnDestroy,
     OnInit,
     Optional,
 } from "@angular/core";
 import { ANIMATION_MODULE_TYPE } from "@angular/platform-browser/animations";
+import { Device } from "../../shared/interfaces/device.interface";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 import { MatButtonModule } from "@angular/material/button";
 import { Footer } from "../../shared/footer/footer";
@@ -20,8 +25,19 @@ import { MatCardModule } from "@angular/material/card";
 import { GuideItems } from "../../shared/guide-items/guide-items";
 import { NgFor } from "@angular/common";
 import { YouTubePlayer } from "@angular/youtube-player";
+import { InventoryService } from "../../shared/services/inventory.service";
 
 import { Carousel, CarouselItem } from "../../shared/carousel/carousel";
+import {
+    Color,
+    NgxChartsModule,
+    PieChartModule,
+    ScaleType,
+} from "@swimlane/ngx-charts";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { JobService } from "../../shared/services/job.service";
+import { Job } from "../../shared/interfaces/job.interface";
 
 @Component({
     selector: "app-homepage",
@@ -40,22 +56,301 @@ import { Carousel, CarouselItem } from "../../shared/carousel/carousel";
         MatCardModule,
         YouTubePlayer,
         Footer,
+        NgxChartsModule,
+        PieChartModule,
     ],
 })
-export class Homepage implements OnInit {
+export class Homepage implements OnInit, OnDestroy {
     @HostBinding("class.main-content") readonly mainContentClass = true;
     @HostBinding("class.animations-disabled")
     readonly animationsDisabled: boolean;
+    private destroy$ = new Subject<void>();
+    inventoryItems: Device[] = [];
+
+    // Properties for the PAN-OS and App-ID version charts
+    appIdVersionData: { name: string; value: number }[] = [];
+    panOsVersionData: { name: string; value: number }[] = [];
+    view: [number, number] = [700, 400];
+    gradient = true;
+    colorScheme: Color = {
+        name: "custom",
+        selectable: true,
+        group: ScaleType.Ordinal,
+        domain: [
+            "#04B3D9",
+            "#AE6321",
+            "#D96704",
+            "#248FA6",
+            "#84562F",
+            "#306773",
+            "#59422E",
+            "#283B40",
+            "#332A23",
+            "#2A3133",
+            "#D9D0C3",
+            "#C2DCF2",
+            "#FFE1B3",
+            "#A2D3FF",
+            "#FFCE80",
+            "#735D39",
+            "#303D48",
+            "#395873",
+            "#40382D",
+            "#A6B8C8",
+        ],
+    };
+
+    // Properties for the grouped app_version chart
+    appVersionByDeviceGroupData: any[] = [];
+    appVersionChartView: [number, number] = [700, 400];
+    appVersionLegendTitle: string = "App-ID Version";
+    appVersionShowLegend: boolean = true;
+    appVersionShowXAxis: boolean = true;
+    appVersionShowYAxis: boolean = true;
+    appVersionShowXAxisLabel: boolean = true;
+    appVersionShowYAxisLabel: boolean = true;
+    appVersionXAxisLabel: string = "Count";
+    appVersionYAxisLabel: string = "Device Group";
+
+    // Properties for the Job Data chart
+    jobData: any[] = [];
+    jobChartView: [number, number] = [700, 400];
+    jobLegendTitle: string = "Job Status";
+    jobShowLegend: boolean = true;
+    jobShowXAxis: boolean = true;
+    jobShowXAxisLabel: boolean = true;
+    jobShowYAxis: boolean = true;
+    jobShowYAxisLabel: boolean = true;
+    jobXAxisLabel: string = "Count";
+    jobYAxisLabel: string = "Job Type";
+
+    // Properties for the grouped sw_version chart
+    swVersionByDeviceGroupData: any[] = [];
+    swVersionChartView: [number, number] = [700, 400];
+    swVersionLegendTitle: string = "PAN-OS Version";
+    swVersionShowLegend: boolean = true;
+    swVersionShowXAxis: boolean = true;
+    swVersionShowYAxis: boolean = true;
+    swVersionShowXAxisLabel: boolean = true;
+    swVersionShowYAxisLabel: boolean = true;
+    swVersionXAxisLabel: string = "Count";
+    swVersionYAxisLabel: string = "Device Group";
 
     constructor(
         public _componentPageTitle: ComponentPageTitle,
         public guideItems: GuideItems,
+        private inventoryService: InventoryService,
+        private jobService: JobService,
+        private snackBar: MatSnackBar,
         @Optional() @Inject(ANIMATION_MODULE_TYPE) animationsModule?: string,
     ) {
         this.animationsDisabled = animationsModule === "NoopAnimations";
     }
 
+    /**
+     * Retrieves devices from the inventory service and updates the component's inventoryItems and dataSource properties
+     *
+     * @returns
+     */
+    getDevices(): void {
+        this.inventoryService
+            .getDevices()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(
+                (items) => {
+                    this.inventoryItems = items;
+                    this.inventoryItems.sort((a, b) =>
+                        a.hostname.localeCompare(b.hostname),
+                    );
+                    this.updatePanOsVersionChart();
+                    this.updateAppIdVersionChart();
+                    this.updateSwVersionByDeviceGroupChart();
+                    this.updateAppVersionByDeviceGroupChart();
+                },
+                (error) => {
+                    console.error("Error fetching inventory items:", error);
+                    this.snackBar.open(
+                        "Failed to fetch inventory items. Please try again.",
+                        "Close",
+                        {
+                            duration: 3000,
+                        },
+                    );
+                },
+            );
+    }
+
+    getJobs(): void {
+        this.jobService
+            .getJobs()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(
+                (jobs) => {
+                    this.prepareJobChartData(jobs);
+                },
+                (error) => {
+                    console.error("Error fetching jobs:", error);
+                    this.snackBar.open(
+                        "Failed to fetch jobs. Please try again.",
+                        "Close",
+                        {
+                            duration: 3000,
+                        },
+                    );
+                },
+            );
+    }
+
+    /**
+     * Method to destroy the component and clean up resources.
+     * It completes the subject 'destroy$' and emits the complete signal using 'next()' method.
+     *
+     * @returns Indicates that the component has been destroyed.
+     */
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
     ngOnInit(): void {
-        this._componentPageTitle.title = "";
+        this._componentPageTitle.title = "Dashboard";
+        this.getDevices();
+        this.getJobs();
+    }
+
+    onSelect(event: any): void {
+        console.log("Item clicked", JSON.parse(JSON.stringify(event)));
+    }
+
+    onActivate(event: any): void {
+        console.log("Activate", JSON.parse(JSON.stringify(event)));
+    }
+
+    onDeactivate(event: any): void {
+        console.log("Deactivate", JSON.parse(JSON.stringify(event)));
+    }
+
+    updatePanOsVersionChart(): void {
+        const versionCounts = new Map<string, number>();
+
+        this.inventoryItems.forEach((device: Device) => {
+            if (device.sw_version) {
+                const count: number = versionCounts.get(device.sw_version) || 0;
+                versionCounts.set(device.sw_version, count + 1);
+            }
+        });
+
+        this.panOsVersionData = Array.from(versionCounts, ([name, value]) => ({
+            name,
+            value,
+        }));
+    }
+
+    updateAppIdVersionChart(): void {
+        const versionCounts = new Map<string, number>();
+
+        this.inventoryItems.forEach((device: Device): void => {
+            if (device.app_version) {
+                const count: number =
+                    versionCounts.get(device.app_version) || 0;
+                versionCounts.set(device.app_version, count + 1);
+            }
+        });
+
+        this.appIdVersionData = Array.from(versionCounts, ([name, value]) => ({
+            name,
+            value,
+        }));
+    }
+
+    prepareJobChartData(jobs: Job[]): void {
+        const jobTypeMap = new Map<string, Map<string, number>>();
+
+        jobs.forEach((job) => {
+            if (!jobTypeMap.has(job.job_type)) {
+                jobTypeMap.set(job.job_type, new Map<string, number>());
+            }
+            const statusMap = jobTypeMap.get(job.job_type)!;
+            statusMap.set(
+                job.job_status,
+                (statusMap.get(job.job_status) || 0) + 1,
+            );
+        });
+
+        this.jobData = Array.from(jobTypeMap.entries()).map(
+            ([jobType, statusMap]) => ({
+                name: jobType,
+                series: Array.from(statusMap.entries()).map(
+                    ([status, count]) => ({
+                        name: status,
+                        value: count,
+                    }),
+                ),
+            }),
+        );
+    }
+
+    updateAppVersionByDeviceGroupChart(): void {
+        const groupData = new Map<string, Map<string, number>>();
+
+        this.inventoryItems.forEach((device: Device) => {
+            if (device.device_group && device.app_version) {
+                if (!groupData.has(device.device_group)) {
+                    groupData.set(
+                        device.device_group,
+                        new Map<string, number>(),
+                    );
+                }
+                const versionMap = groupData.get(device.device_group)!;
+                versionMap.set(
+                    device.app_version,
+                    (versionMap.get(device.app_version) || 0) + 1,
+                );
+            }
+        });
+
+        this.appVersionByDeviceGroupData = Array.from(groupData.entries()).map(
+            ([deviceGroup, versionMap]) => ({
+                name: deviceGroup,
+                series: Array.from(versionMap.entries()).map(
+                    ([version, count]) => ({
+                        name: version,
+                        value: count,
+                    }),
+                ),
+            }),
+        );
+    }
+
+    updateSwVersionByDeviceGroupChart(): void {
+        const groupData = new Map<string, Map<string, number>>();
+
+        this.inventoryItems.forEach((device: Device) => {
+            if (device.device_group && device.sw_version) {
+                if (!groupData.has(device.device_group)) {
+                    groupData.set(
+                        device.device_group,
+                        new Map<string, number>(),
+                    );
+                }
+                const versionMap = groupData.get(device.device_group)!;
+                versionMap.set(
+                    device.sw_version,
+                    (versionMap.get(device.sw_version) || 0) + 1,
+                );
+            }
+        });
+
+        this.swVersionByDeviceGroupData = Array.from(groupData.entries()).map(
+            ([deviceGroup, versionMap]) => ({
+                name: deviceGroup,
+                series: Array.from(versionMap.entries()).map(
+                    ([version, count]) => ({
+                        name: version,
+                        value: count,
+                    }),
+                ),
+            }),
+        );
     }
 }
