9d5ccb15e1c1c18022e57744822d5a20a2084ddd
Calvin Remsburg <calvin@cdot.io>
2024-07-20 11:02:36 -0500

    Refactor upgrade-diagram component to use UpgradeStepService
    The upgrade-diagram component has been modified to use UpgradeStepService for fetching the SVG path of the current step. As part of this change, the component no longer implements OnChanges. The SVG path is now obtained asynchronously, with image loading and visibility controlled through async pipe in the template. This change simplifies code and improves component efficiency.

--- frontend/src/app/pages/upgrade-diagram/upgrade-diagram.component.ts
+++ frontend/src/app/pages/upgrade-diagram/upgrade-diagram.component.ts
@@ -1,11 +1,7 @@
 // src/app/pages/upgrade-diagram/upgrade-diagram.component.ts
-import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
-interface StepMapping {
-    name: string;
-    svgPath: string;
-}
+import { UpgradeStepService } from "../../shared/services/upgrade-step.service";
 
 @Component({
     selector: "app-upgrade-diagram",
@@ -14,66 +10,8 @@ interface StepMapping {
     templateUrl: "./upgrade-diagram.component.html",
     styleUrls: ["./upgrade-diagram.component.scss"],
 })
-export class UpgradeDiagramComponent implements OnChanges {
-    @Input() currentStep: string = "";
+export class UpgradeDiagramComponent {
+    currentStepSvgPath$ = this.upgradeStepService.getCurrentStepSvgPath();
 
-    readonly stepMappings: StepMapping[] = [
-        {
-            name: "Validate Upgrade Path",
-            svgPath: "assets/img/site/validate-upgrade-path.svg",
-        },
-        {
-            name: "s",
-            svgPath: "assets/img/site/image-version-validation.svg",
-        },
-        {
-            name: "Download Base Image",
-            svgPath: "assets/img/site/download-base-image.svg",
-        },
-        {
-            name: "Download Target Image",
-            svgPath: "assets/img/site/download-target-image.svg",
-        },
-        {
-            name: "Readiness Checks",
-            svgPath: "assets/img/site/readiness-checks.svg",
-        },
-        {
-            name: "Pre-Upgrade Snapshot",
-            svgPath: "assets/img/site/pre-upgrade-snapshot.svg",
-        },
-        {
-            name: "HA State Suspension",
-            svgPath: "assets/img/site/ha-state-suspension.svg",
-        },
-        {
-            name: "Upgrade PAN-OS",
-            svgPath: "assets/img/site/upgrade-pan-os.svg",
-        },
-        {
-            name: "Reboot and Wait",
-            svgPath: "assets/img/site/reboot-and-wait.svg",
-        },
-        {
-            name: "Post-Upgrade Snapshot",
-            svgPath: "assets/img/site/post-upgrade-snapshot.svg",
-        },
-        {
-            name: "Upgrade Complete",
-            svgPath: "assets/img/site/upgrade-complete.svg",
-        },
-    ];
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes["currentStep"]) {
-            this.getCurrentStepSvgPath();
-        }
-    }
-
-    getCurrentStepSvgPath(): string {
-        const currentStepMapping = this.stepMappings.find(
-            (step) => step.name === this.currentStep,
-        );
-        return currentStepMapping ? currentStepMapping.svgPath : "";
-    }
+    constructor(private upgradeStepService: UpgradeStepService) {}
 }
